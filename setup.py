0d378b79ec18b0d3df354bec38e897d6aec6b7e9
Normf2000 <normf2000@gmail.com>
2025-03-07 19:54:02 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kanorai',
+    version='2.0',
+    packages=find_packages(),
+    install_requires=[
+        'Scrapy>=2.11',
+        'pydantic>=2.6',
+        'scrapy-zyte-smartproxy>=1.2',
+    ],
+    entry_points={'scrapy': ['settings = kanorai.settings']},
+)
