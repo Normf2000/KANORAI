0be9a17e8d889244ed57bd9e90d69346122f3972
Normf2000 <normf2000@gmail.com>
2025-03-08 20:41:14 +0200

    Update settings.py

--- kanorai/settings.py
+++ kanorai/settings.py
@@ -4,30 +4,30 @@ import os
 # Add project directory to Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-BOT_NAME = 'kanorai'
-SPIDER_MODULES = ['kanorai.spiders']
-NEWSPIDER_MODULE = 'kanorai.spiders'
+BOT_NAME = "kanorai"
+SPIDER_MODULES = ["kanorai.spiders"]
+NEWSPIDER_MODULE = "kanorai.spiders"
 
 # Zyte Smart Proxy settings
 ZYTE_SMARTPROXY_ENABLED = True
 ZYTE_SMARTPROXY_APIKEY = "c531770e0c3b4db793a517d3a001a341"
-ZYTE_SMARTPROXY_URL = 'http://api.zyte.com:8011'
+ZYTE_SMARTPROXY_URL = "http://api.zyte.com:8011"  # Correct proxy URL
 
 DOWNLOADER_MIDDLEWARES = {
-    'scrapy_zyte_smartproxy.ZyteSmartProxyMiddleware': 610,
+    "scrapy_zyte_smartproxy.ZyteSmartProxyMiddleware": 610,
 }
 
 ITEM_PIPELINES = {
-    'kanorai.pipelines.ValidationPipeline': 300,
-    'kanorai.pipelines.ExportPipeline': 800,
+    "kanorai.pipelines.ValidationPipeline": 300,
+    "kanorai.pipelines.ExportPipeline": 800,
 }
 
 FEEDS = {
-    'apartments.json': {
-        'format': 'json',
-        'encoding': 'utf8',
-        'indent': 2,
-        'overwrite': True
+    "apartments.json": {
+        "format": "json",
+        "encoding": "utf8",
+        "indent": 2,
+        "overwrite": True
     }
 }
 
@@ -37,11 +37,11 @@ CONCURRENT_REQUESTS_PER_DOMAIN = 2
 DOWNLOAD_DELAY = 1.5  # Reduced from 2 seconds
 AUTOTHROTTLE_ENABLED = True
 RETRY_TIMES = 5
-RETRY_HTTP_CODES = [407, 429, 503]  # Corrected setting
+RETRY_HTTP_CODES = [407, 429, 503]
 RETRY_PRIORITY_ADJUST = -1
 
 # Save logs to a file
-LOG_FILE = 'scrapy_log.txt'
+LOG_FILE = "scrapy_log.txt"
 
-# Update deprecated setting
-REQUEST_FINGERPRINTER_IMPLEMENTATION = '2.7'
+# Correct setting for REQUEST_FINGERPRINTER_IMPLEMENTATION
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
