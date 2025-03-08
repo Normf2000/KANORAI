80c3ff3fb00d551d4d5d28173abc92d110a00321
Normf2000 <normf2000@gmail.com>
2025-03-08 20:29:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='kanorai',
-    version='2.0',
+    name="kanorai",
+    version="2.0",
     packages=find_packages(),
     install_requires=[
-        'Scrapy>=2.11',
-        'pydantic>=2.6',
-        'scrapy-zyte-smartproxy>=1.2',
+        "Scrapy>=2.11",
+        "pydantic>=2.6",
+        "scrapy-zyte-smartproxy>=1.2",
     ],
-    entry_points={'scrapy': ['settings = kanorai.settings']},
-    python_requires='>=3.7',
+    entry_points={"scrapy": ["settings = kanorai.settings"]},
+    python_requires=">=3.7",
 )
